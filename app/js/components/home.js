82b6da28eb95a7dc4a640be166b3e6c11120fb76
yrouille <yrouille@hachette-livre.fr>
2016-05-30 19:23:31 +0200

    ngModel

--- app/js/components/home.js
+++ app/js/components/home.js
@@ -4,57 +4,77 @@ var app = angular.module('app');
 
 app.controller('homeController', ['$scope', '$http',
   function ($scope, $http) {
-    $scope.data = {};
-    $scope.data.date = moment().format();
+    $scope.master = {};
 
-    $scope.data.starttime = moment().hours('10').minutes('00').seconds('00');
-    $scope.data.endtime = moment().hours('11').minutes('00').seconds('00');
+    $scope.update = function (booking) {
+//      $scope.form.theme.$invalid = true;
 
-    $scope.formIsValid = false;
-    $scope.errorsDetected = false;
+      $scope.master = angular.copy(booking);
+    };
 
-
-
-    $scope.$watch('data.starttime', function (newValue, oldValue) {
-      
-//      console.log('data.starttime', $scope.data.starttime.hours());
-//      console.log('data.starttime', $scope.data.starttime.hours() + 1);
-      
-      var aze = $scope.data.starttime.hours() + 1;
-      $scope.data.endtime.hours(aze);
-      
-    });
-
-
-
-    $scope.submitForm = function () {
-      console.log('submitForm', $scope.bookingForm.$valid);
-
-      if ($scope.bookingForm.$valid) {
-        $scope.formIsValid = true;
-        $scope.errorsDetected = false;
-
-        console.log('success', $scope.data);
-        // $http.post('/request.php', $scope.data);
-
-      } else {
-        $scope.formIsValid = false;
-        $scope.errorsDetected = true;
+    $scope.reset = function (form) {
+      if (form) {
+        form.$setPristine();
+        form.$setUntouched();
       }
+      $scope.booking = angular.copy($scope.master);
     };
 
+    $scope.reset();
 
 
 
 
-    $scope.resetForm = function () {
-      $scope.formIsValid = false;
-      $scope.errorsDetected = false;
-
-      console.log('resetForm');
-    };
 
 
 
+
+
+
+
+//    $scope.data = {};
+//    $scope.data.date = moment().format();
+//
+//    $scope.data.starttime = moment().hours('10').minutes('00').seconds('00');
+//    $scope.data.endtime = moment().hours('11').minutes('00').seconds('00');
+//
+//    $scope.formIsValid = false;
+//    $scope.errorsDetected = false;
+//
+//
+//    $scope.$watch('data.starttime', function (newValue, oldValue) {
+//      
+////      console.log('data.starttime', $scope.data.starttime.hours());
+////      console.log('data.starttime', $scope.data.starttime.hours() + 1);
+//      
+//      var aze = $scope.data.starttime.hours() + 1;
+//      $scope.data.endtime.hours(aze);
+//      
+//    });
+//
+//
+//
+//    $scope.submitForm = function () {
+//      console.log('submitForm', $scope.bookingForm.$valid);
+//
+//      if ($scope.bookingForm.$valid) {
+//        $scope.formIsValid = true;
+//        $scope.errorsDetected = false;
+//
+//        console.log('success', $scope.data);
+//        // $http.post('/request.php', $scope.data);
+//
+//      } else {
+//        $scope.formIsValid = false;
+//        $scope.errorsDetected = true;
+//      }
+//    };
+//
+//    $scope.resetForm = function () {
+//      $scope.formIsValid = false;
+//      $scope.errorsDetected = false;
+//
+//      console.log('resetForm');
+//    };
 
   }]);
--- app/js/shared/form.js
+++ app/js/shared/form.js
@@ -3,7 +3,6 @@
 var app = angular.module('app');
 
 var integerRegex = /^\-?\d+$/;
-
 app.directive('integer', function () {
   return {
     require: 'ngModel',
@@ -25,3 +24,44 @@ app.directive('integer', function () {
     }
   };
 });
+
+
+
+
+
+app.directive('aze', function () {
+  return {
+    require: 'ngModel',
+    templateUrl: 'views/datepicker.html',
+    link: function (scope, elm, attrs, ctrl) {
+      elm.datetimepicker({
+        defaultDate: 'moment',
+        minDate: 'moment',
+        format: 'L',
+        locale: 'fr'
+      });
+
+      scope.booking.hello = elm.data('DateTimePicker').date();
+
+      scope.$watch('booking.hello', function (newVal, oldVal) {
+//        console.log('Hello', 'World', newVal);
+
+//        if (newVal.format('L') !== oldVal.format('L')) {
+          console.log('newVal', newVal.format('L'));
+//          console.log('oldVal', oldVal.format('L'));
+//        }
+
+      });
+
+      elm.on('dp.change', function (event) {
+        scope.booking.hello = event.date;
+        scope.$apply();
+      });
+    }
+  };
+});
+
+
+
+
+
